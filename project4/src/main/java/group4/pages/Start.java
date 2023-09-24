7c0a5a2f0ba5e9f446dbd0d605bb13976b309e9e
samathotimanoj <manojsamathoti.26@gmail.com>
2023-09-24 21:13:21 +0530

    project file

---
+++ project4/src/main/java/group4/pages/Start.java
@@ -0,0 +1,14 @@
+package group4.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application project4.
+ */
+public class Start
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
